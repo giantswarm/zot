aeb10f7e873c42ca2275506eb23609f4efc22313
Łukasz Piątkowski <piontec@gmail.com>
2024-03-11 10:41:06 +0100

    run upgrade script as gh action - fix 6

--- .github/workflows/ci-update-to-latest.sh
+++ .github/workflows/ci-update-to-latest.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 source subtree-cfg.sh
 
@@ -18,6 +18,7 @@ if [[ $remote_status -eq 0 ]]; then
 	echo "Detected correct remote upstream URL: $remote_url"
 fi
 
+set -e
 if [[ "$remote_ok" = false ]]; then
 	echo "Adding remote upstream"
 	git remote add upstream $REMOTE_URL
