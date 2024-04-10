52ee4fe88740cd7b5c482fafbe9210857dd0c938
Łukasz Piątkowski <piontec@gmail.com>
2024-04-10 14:44:21 +0200

    add post-hook for git-subtree-itls

---
+++ post-hook.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE=Chart.yaml
+FILE_PATH=helm/zot/"$FILE"
+OURS=/tmp/"$FILE".ours
+THEIRS=/tmp/"$FILE".theirs
+
+echo "Patching $FILE with upstream versions info"
+git show :2:"$FILE_PATH" >"$OURS"
+git show :3:"$FILE_PATH" >"$THEIRS"
+
+yq ".appVersion = load(\"$THEIRS\").appVersion" -i "$OURS"
+yq ".version = load(\"$THEIRS\").version" -i "$OURS"
+yq ".upstreamChartVersion = load(\"$THEIRS\").version" -i "$OURS"
+
+cp "$OURS" "$FILE_PATH"
